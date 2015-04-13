f3ec1ff8d95fc903fbad6ab3175983cd58ee6f21
Joris Kraak <me@joriskraak.nl>
2015-04-13 16:38:07 +0200

    chore(bsingo): allow ES6 through BabelJS

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   context: __dirname + '/src',
   entry: './index.js',
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel', exclude: /node_modules/ }
+    ]
+  },
   output: {
     path: __dirname + '/dist',
     filename: 'bundle.js'
